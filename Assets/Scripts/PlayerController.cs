d0fb87f2e4df259b81a771395ed3c3fcf8783951
FIFAIFO <jeffreyj75@nycstudents.net>
2024-11-21 09:01:43 -0500

    completed part 9

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,32 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
 
     public InputAction MoveAction;
-    private Rigidbody
+    Vector2 move;
+    private Rigidbody2D rigidbody2d;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
         MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
+        move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
-       
-
-
     }
-    void Fixupdate()
+    void FixedUpdate()
     {
 
-          Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
-        transform.position = position;
+          Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+            rigidbody2d.MovePosition(position);
     }
 
 
