67bca457aaf2861809f1f0e774efe7b0999d2333
FIFAIFO <jeffreyj75@nycstudents.net>
2024-12-18 09:22:34 -0500

    part 12 complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,20 +9,22 @@ public class PlayerController : MonoBehaviour
     public InputAction MoveAction;
     Vector2 move;
     private Rigidbody2D rigidbody2d;
-    
+    public int maxHealth = 5;
+    int currentHealth;
     
     // Start is called before the first frame update
     void Start()
     {
         MoveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
         move = MoveAction.ReadValue<Vector2>();
-        Debug.Log(move);
+
     }
     void FixedUpdate()
     {
@@ -31,26 +33,9 @@ public class PlayerController : MonoBehaviour
             rigidbody2d.MovePosition(position);
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public void ChangeHealth (int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
 }
